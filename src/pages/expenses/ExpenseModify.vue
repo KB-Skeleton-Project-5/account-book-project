cedbd201f098f9a998e5b17e8dfc08c94cb1441d
ryeon-xx <rlarkgus3415@gmail.com>
2026-04-09 21:20:26 +0900

    feat: Update 기능 구현 완료

--- src/pages/expenses/ExpenseModify.vue
+++ src/pages/expenses/ExpenseModify.vue
@@ -1,10 +1,17 @@
 <template>
     <DefaultLayout>
         <template #header>
-            <AppHeader title="수정" :back="true" backTo="expenses/info/id"/>
+            <AppHeader 
+            title="수정" 
+            :back="true" backTo="expenses/info/id"/>
         </template>
-        <ExpenseForm />
-        <AppButton text="저장" @click="handleSave" />
+        <ExpenseForm 
+        :initialData="expenseData"
+        ref="expenseFormRef"
+        @submit-form="handleSubmit"/>
+        <AppButton 
+        text="저장" 
+        @click="handleSave" />
 
         <template #footer>
             <AppFooter />
@@ -16,14 +23,33 @@
 import DefaultLayout from '@/layouts/DefaultLayout.vue';
 import ExpenseForm from '@/components/expenses/ExpenseForm.vue'
 import AppButton from '@/components/commons/AppButton.vue'
-import { useRouter } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import AppHeader from '@/layouts/AppHeader.vue';
 import AppFooter from '@/layouts/AppFooter.vue';
+import { ref, onMounted } from 'vue';
+import { getExpense, updateExpenses } from '@/api/expenses';
 
 const router = useRouter();
+const route = useRoute();
 
+const expenseData = ref(null);
+const expenseFormRef = ref(null);
+
+// 1. 페이지 들어오면 id로 데이터 불러오기
+onMounted(async () => {
+    const response = await getExpense(route.params.id);
+    expenseData.value = response.data;
+})
+
+// 2. 저장 버튼 클릭 -> ExpenseForm한테 데이터 달라고 요청
 const handleSave = () => {
-    router.push({ name : 'expenses' });
+    expenseFormRef.value.submitForm();
+}
+
+// 3. ExpenseForm에서 데이터 받으면 -> API 호출 후 목록으로 이동
+const handleSubmit = async(data) => {
+    await updateExpenses(route.params.id, data);
+    router.push({name : 'expenses'});
 }
 </script>
 
