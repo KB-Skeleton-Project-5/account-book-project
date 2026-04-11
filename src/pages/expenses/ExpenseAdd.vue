7ff983ccb13b288bcdbdf794f9bc27c375858b7d
ryeon-xx <rlarkgus3415@gmail.com>
2026-04-12 03:43:29 +0900

    CSS 작업 완료

--- src/pages/expenses/ExpenseAdd.vue
+++ src/pages/expenses/ExpenseAdd.vue
@@ -5,8 +5,14 @@
       <AppHeader title="거래 내역 등록" :back="true" backTo="expenses"/>
     </template>
 
-    <ExpenseForm ref="expenseFormRef" @submit-form="handleSubmit" />
-    <AppButton text="저장" @click="handleSave" />
+    <div class="add-container">
+      <div class="expense-add-card">
+        <ExpenseForm ref="expenseFormRef" @submit-form="handleSubmit" />
+        <div class="add-action-buttons">
+          <AppButton text="저장" @click="handleSave" />
+        </div>
+      </div>
+    </div>
 
     <template #footer>
       <AppFooter />
@@ -25,24 +31,45 @@ import { ref } from 'vue';
 import axios from 'axios';
 import { getUserInfo } from '@/util/authUtil.js';
 
-// 현재 로그인한 유저 정보 가져오는 유틸 함수
 const userInfo = getUserInfo();
-
 
 const router = useRouter();
 const expenseFormRef = ref(null)
 
 const handleSave = () => {
-  expenseFormRef.value.submitForm()   // 자식 함수 직접 호출
+  expenseFormRef.value.submitForm()
 }
 
 const handleSubmit = async (formData) => {
   try{
-    // console.log('저장 데이터 : ', formData);
     await axios.post('/api/expenses', {...formData, user_id: String(userInfo.id)})
     router.push({ name: 'expenses' })
   } catch (e) {
-    console.error('저장 실패 : ', e); 
+    console.error('저장 실패 : ', e);
   }
 };
 </script>
+
+<style scoped>
+.add-container {
+  padding: 20px 0;
+}
+
+.expense-add-card {
+  width: calc(100% - 40px);
+  max-width: 400px;
+  background-color: #ffffff;
+  border-radius: 20px;
+  padding: 20px 8px;
+  margin: 0 auto;
+  box-shadow: 0 15px 45px rgba(0, 0, 0, 0.08);
+  box-sizing: border-box;
+}
+
+.add-action-buttons {
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 16px;
+  padding-right: 16px;
+}
+</style>
